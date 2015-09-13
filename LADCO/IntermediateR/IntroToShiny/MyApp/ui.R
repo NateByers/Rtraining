bca243817711c130f3066fb95d44d96a1a0a7073
Nathan Byers <natebyers@gmail.com>
2015-09-13 10:07:32 -0400

    added shiny tutorial and readme

---
+++ LADCO/IntermediateR/IntroToShiny/MyApp/ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+# Define UI for application that plots time series
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Chicago Air"),
+  
+  # Sidebar with a dropdown for selecting parameter
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("parameter",
+                  "Select Parameter:",
+                  c("Ozone" = "ozone",
+                  "Temperature" = "temp",
+                  "Solar Radiation" = "solar"))
+    ),
+    
+    # Show a plot of the time series
+    mainPanel(
+      plotOutput("timePlot")
+    )
+  )
+))
---
+++ README.md
@@ -0,0 +1,14 @@
+# Rtraining
+
+Materials for R training.
+
+## Intermediate R
+
+1. Data Manipulation: dplyr
+2. Data Manipluation: dplyr and tidyr
+3. Merging: base and dplyr
+4. Functions and apply()
+5. Plotting with openair
+6. Spatial Data Objects
+7. Space-Time Data Objects
+8. Shiny
